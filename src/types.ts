c7bd44514ec4486e1875e53ba9c953c688429e42
Kirana-prit <kirana_pritsana@cmu.ac.th>
2025-07-04 09:51:33 +0700

    8.4

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface Event {
+    id: number;
+    category: string;
+    title: string;
+    description: string;
+    date: string;
+    time: string;
+    location: string;
+    petsAllowed: boolean;
+    organizer: string;
+}
