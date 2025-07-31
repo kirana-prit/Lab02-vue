e8b0436431f137af016cac6cc34c1c9d2beac6fb
Kirana-prit <kirana_pritsana@cmu.ac.th>
2025-08-01 02:36:00 +0700

    1.7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
+  darkMode: false, 
   variants: {
     extend: {},
   },
