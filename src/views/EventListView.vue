21cd1ff37d3c4caf3bea5255e096213cd2bccb79
Kirana-prit <kirana_pritsana@cmu.ac.th>
2025-08-05 02:41:07 +0700

    2.3

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import EventCard from '@/components/EventCard.vue';
   import  {type Event } from '@/types';
-  import { ref, onMounted, computed, watchEffect,watch } from 'vue';
+  import { ref, onMounted, computed,watch } from 'vue';
   import EventService from '@/services/EventService';
   import { useRoute } from 'vue-router';
   const events = ref<Event[] | null>(null);
@@ -33,10 +33,10 @@ const loadEvents = () => {
 </script>
 
 <template>
-  
+
   <h1>Events For Good</h1>
   <!-- new element -->
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <div class="pagination">
       <RouterLink id="page-prev" :to="{ name: 'event-list-view', query: { page: page - 1,size:size } }"
@@ -66,11 +66,6 @@ const loadEvents = () => {
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 .pagination{
   display: flex;
   width: 290px;
